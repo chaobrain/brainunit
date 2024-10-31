6c65a84a04cfd0e95aabae26c0414cfb16bc012e
He Sichao <1310722434@qq.com>
2024-10-31 20:50:22 +0800

    Try add more registry

--- brainunit/_matplotlib_compat.py
+++ brainunit/_matplotlib_compat.py
@@ -20,8 +20,9 @@ from typing import List
 
 import numpy as np
 from brainstate.typing import ArrayLike
+from numpy import ma
 
-from ._base import Quantity, fail_for_dimension_mismatch, UNITLESS
+from ._base import Quantity, fail_for_dimension_mismatch, UNITLESS, UnitMismatchError
 
 matplotlib_installed = importlib.util.find_spec('matplotlib') is not None
 
@@ -30,25 +31,25 @@ __all__ = [
 ]
 
 if matplotlib_installed:
-  from matplotlib import ticker, units, pyplot as plt
+  from matplotlib import ticker, units, axis, pyplot as plt
   from matplotlib.lines import Line2D
 
-  setattr(plt, "_plot", plt.plot)
-
-
-  def plotq(
-      *args: float | ArrayLike | str,
-      scalex: bool = True,
-      scaley: bool = True,
-      data=None,
-      **kwargs,
-  ) -> List[Line2D]:
-    # args to Quantity
-    args = [arg if isinstance(arg, Quantity) else Quantity(arg) for arg in args]
-    return plt._plot(*args, scalex=scalex, scaley=scaley, data=data, **kwargs)
-
-
-  setattr(plt, "plot", plotq)
+  # setattr(plt, "_plot", plt.plot)
+  #
+  #
+  # def plotq(
+  #     *args: float | ArrayLike | str,
+  #     scalex: bool = True,
+  #     scaley: bool = True,
+  #     data=None,
+  #     **kwargs,
+  # ) -> List[Line2D]:
+  #   # args to Quantity
+  #   args = [arg if isinstance(arg, Quantity) else Quantity(arg) for arg in args]
+  #   return plt._plot(*args, scalex=scalex, scaley=scaley, data=data, **kwargs)
+  #
+  #
+  # setattr(plt, "plot", plotq)
 
 
   def rad_fn(
@@ -92,7 +93,36 @@ if matplotlib_installed:
         fail_for_dimension_mismatch(val[0].unit, unit)
         return [v.to(unit).mantissa if v.unit != unit else v.mantissa for v in val]
       else:
-        return val
+        if unit is UNITLESS:
+          return val
+        else:
+          raise UnitMismatchError(UNITLESS, unit)
+
+    @staticmethod
+    def default_units(x, axis):
+      if hasattr(x, "unit"):
+        return x.unit
+      return UNITLESS
+
+  # check decimal
+
+  class DecimalConverter(units.ConversionInterface):
+    @staticmethod
+    def convert(value, unit, axis):
+      # check value unit is UNITLESS
+      if isinstance(value, Quantity):
+        if value.unit is UNITLESS:
+          raise UnitMismatchError(value.unit, UNITLESS)
+        return value.mantissa
+
+      if isinstance(value, units.Decimal):
+        result = float(value)
+      # value is Iterable[Decimal]
+      elif isinstance(value, ma.MaskedArray):
+        result = ma.asarray(value, dtype=float)
+      else:
+        result = np.asarray(value, dtype=float)
+      return Quantity(result)
 
     @staticmethod
     def default_units(x, axis):
@@ -101,6 +131,26 @@ if matplotlib_installed:
       return UNITLESS
 
 
+  def convert_units(self, x):
+    # If x is natively supported by Matplotlib, doesn't need converting
+    if units._is_natively_supported(x):
+      return x
+
+    if self.converter is None:
+      self.converter = units.registry.get_converter(x)
+
+    if self.converter is None:
+      return x
+    try:
+      ret = self.converter.convert(x, self.units, self)
+    except Exception as e:
+      raise units.ConversionError('Failed to convert value(s) to axis '
+                                   f'units: {x!r}') from e
+    return ret
+
+  axis.Axis.convert_units = convert_units
+  units.registry[units.Decimal] = DecimalConverter()
+  units.registry[np.number] = DecimalConverter()
   units.registry[Quantity] = MplQuantityConverter()
 
 
