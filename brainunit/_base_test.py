9075f403857aec675c39c152d0cda15f6b7c8b1b
Chaoming Wang <adaduo@outlook.com>
2024-12-03 21:42:14 +0800

    add more tests

--- brainunit/_base_test.py
+++ brainunit/_base_test.py
@@ -30,7 +30,6 @@ import pytest
 from numpy.testing import assert_equal
 
 import brainunit as u
-import brainunit as bu
 from brainunit._base import (
     DIMENSIONLESS,
     UNITLESS,
@@ -1245,6 +1244,7 @@ class TestHelperFunctions(unittest.TestCase):
             c_function(1, 1)
         with pytest.raises(TypeError):
             c_function(1 * mV, 1)
+
         # with pytest.raises(TypeError):
         #     c_function(False, 1)
 
@@ -1265,6 +1265,28 @@ class TestHelperFunctions(unittest.TestCase):
         # Should fail (returns volt)
         with pytest.raises(u.DimensionMismatchError):
             d_function(False)
+
+        # Multiple results
+        @u.check_dims(result={'u': second.dim, 'v': (volt.dim, metre.dim)})
+        def d_function2(true_result):
+            """
+            Return a value in seconds if return_second is True, otherwise return
+            a value in volt.
+            """
+            if true_result == 0:
+                return {'u': 5 * second, 'v': (3 * volt, 2 * metre)}
+            elif true_result == 1:
+                return 3 * volt, 5 * second
+            else:
+                return {'u': 5 * second, 'v': (3 * volt, 2 * volt)}
+
+        d_function2(0)
+
+        with pytest.raises(TypeError):
+            d_function2(1)
+
+        with pytest.raises(u.DimensionMismatchError):
+            d_function2(2)
 
     def test_check_units(self):
         """
@@ -1350,6 +1372,28 @@ class TestHelperFunctions(unittest.TestCase):
         with pytest.raises(u.UnitMismatchError):
             d_function(False)
 
+        # Multiple results
+        @check_units(result={'u': second, 'v': (volt, metre)})
+        def d_function2(true_result):
+            """
+            Return a value in seconds if return_second is True, otherwise return
+            a value in volt.
+            """
+            if true_result == 0:
+                return {'u': 5 * second, 'v': (3 * volt, 2 * metre)}
+            elif true_result == 1:
+                return 3 * volt, 5 * second
+            else:
+                return {'u': 5 * second, 'v': (3 * volt, 2 * volt)}
+
+        # Should work (returns second)
+        d_function2(0)
+        # Should fail (returns volt)
+        with pytest.raises(TypeError):
+            d_function2(1)
+
+        with pytest.raises(u.UnitMismatchError):
+            d_function2(2)
 
 
 def test_str_repr():
